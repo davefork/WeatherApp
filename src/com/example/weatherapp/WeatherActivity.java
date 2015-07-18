a8811f4d5b60925e55f171c53d0a1ba311605bd8
hxt <503810720@qq.com>
2015-07-18 15:58:53 +0800

    zhong yu jie shu le

--- src/com/example/weatherapp/WeatherActivity.java
+++ src/com/example/weatherapp/WeatherActivity.java
@@ -19,23 +19,12 @@ import com.example.myview.RefreshableView;
 //-------个人包-------//
 public class WeatherActivity extends Activity {
 	private ListView mainList;
-
 	//private RefreshableView refreshView;
-	Gallery g;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sforcast);
-        g=(Gallery) findViewById(R.id.gallery1);
-        try {
-			g.setAdapter(new myAdapter(this));
-		} catch (IllegalArgumentException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
